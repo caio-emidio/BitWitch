5dfe07dcd2538f0b062f0d945a95ac7666f11596
Caio Carnelos <caio.emidio@gmail.com>
2021-05-01 22:42:01 +0100

    Pair Programming com DaviPRM

--- script.js
+++ script.js
@@ -31,3 +31,24 @@ function bitcoinSet(currency, country) {
 bitcoinSet("eur", "eur")
 bitcoinSet("usd", "usd")
 bitcoinSet("brl", "pt-br")
+
+// slider section
+
+// euro, dolar, bitcoin, brl
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("values");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }    
+    x[myIndex-1].style.display = "flex";
+    setTimeout(carousel, 7000);
+}
